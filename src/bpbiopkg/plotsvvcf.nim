4afd0fdaa56f2c058b0d4832677ac1c3e55f1f40
Brent Pedersen <bpederse@gmail.com>
2018-10-06 09:54:02 -0600

    show allele frequency spectrum of each type of variant

--- src/bpbiopkg/plotsvvcf.nim
+++ src/bpbiopkg/plotsvvcf.nim
@@ -1,4 +1,5 @@
 import plotly
+import chroma
 import browsers
 import os
 import hts
@@ -12,6 +13,7 @@ proc toText(samples:seq[string]): seq[string] =
   for i, s in samples:
     result[i] = "sample:" & $s
 
+var afsColor = Color(r: 0.5, g: 0.5, b: 0.5, a:1.0)
 
 proc get_bnd_mate_pos*(a:string, vchrom:string): int {.inline.} =
     if not (':' in a): return -1
@@ -77,11 +79,17 @@ Options:
     inters = {
        "BND": Trace[int](`type`: PlotType.Bar, opacity: 0.8, name:"interchromosomal BNDs", ys: newSeq[int](n)),
     }.toTable
-    counts = {
+    type_counts = {
        "DEL": 0,
        "DUP": 0,
        "INV": 0,
        "BND": 0,
+    }.toTable
+    AFS_counts = {
+      "DEL": Trace[int](`type`: PlotType.Histogram, xs: newSeqOfCap[int](4096), marker:Marker[int](color:(@[afsColor]))),
+      "DUP": Trace[int](`type`: PlotType.Histogram, xs: newSeqOfCap[int](4096), marker:Marker[int](color:(@[afsColor]))),
+      "INV": Trace[int](`type`: PlotType.Histogram, xs: newSeqOfCap[int](4096), marker:Marker[int](color:(@[afsColor]))),
+      "BND": Trace[int](`type`: PlotType.Histogram, xs: newSeqOfCap[int](4096), marker:Marker[int](color:(@[afsColor]))),
     }.toTable
 
   for v in smalls.mvalues:
@@ -98,14 +106,14 @@ Options:
   var k = 0
   for v in vcf:
     k += 1
-    if k mod 10000 == 0:
+    if k mod 50_000 == 0:
       echo $k & " > " & $v.CHROM & ":" & $v.start
       #echo larges["DUP"].ys
     if v.info.get("SVTYPE", svtype) != Status.OK:
        quit "no svtype for:" & $v
     var svlen = v.stop - v.start
     var nalts:seq[int8] = v.format.genotypes(gts).alts
-    counts[svtype].inc
+    type_counts[svtype].inc
     if svtype == "BND":
       var p = get_bnd_mate_pos(v)
       if p == -1:
@@ -118,10 +126,13 @@ Options:
       tr = larges[svtype]
     else:
       tr = smalls[svtype]
+    var n_samples_with_event = 0
     for i, na in nalts:
       if na == 0 or na == -1:
         continue
+      n_samples_with_event += 1
       tr.ys[i] += 1
+    AFS_counts[svtype].xs.add(n_samples_with_event)
 
   var tmpl_hdr = """
 <!DOCTYPE html>
@@ -132,22 +143,29 @@ Options:
    <script src="https://cdn.plot.ly/plotly-latest.min.js"></script>
 </head>
 <body>
+<table>
     """
   var tmpl_body = """
+<td>
       <div id="plot$i"></div>
       <script>
-      var pdata_$i = $data
-      var playout_$i = $layout
-      playout_$i.legend = {orientation: 'h', x:0, y:0}
+      var pdata_$i = $data;
+      var playout_$i = $layout;
+      playout_$i.legend = {orientation: 'h', x:0, y:0};
+      if(Number($i) >= 10) {
+        playout_$i.yaxis.log = true;
+        playout_$i.yaxis.type = "log";
+      }
       var plot$i = Plotly.newPlot('plot$i', pdata_$i, playout_$i)
       </script>
+</td>
     """
 
   var html = tmpl_hdr % ["title", $args["<vcf>"]]
 
   for i, pt in @["DEL", "DUP", "INV", "BND"]:
-    var layout = Layout(width: 1200, height: 360,
-                    yaxis: Axis(title:pt & " (" & $counts[pt] & ")"),
+    var layout = Layout(width: 950, height: 350,
+                    yaxis: Axis(title:pt & " (" & $type_counts[pt] & ")"),
                     xaxis: Axis(title: "sample", hideticklabels:true),
                     barmode: BarMode.Stack,
                     autosize: false)
@@ -159,9 +177,21 @@ Options:
     var j = "[" & join(jsons, ",") & "]"
 
     var h = tmpl_body % ["i", $i, "data", j, "layout", $(%layout)]
-    html &= h
+    html &= "<tr>" & h
 
-  html &= "</body></html>"
+    # AFS plot
+    layout = Layout(width: 950, height: 350,
+                    xaxis: Axis(title: "# of samples with " & pt & "s"),
+                    yaxis: Axis(title: "count of variants"),
+                    autosize: false)
+    jsons = @[AFS_counts[pt].json(as_pretty=false)]
+    j = "[" & join(jsons, ",") & "]"
+
+    h = tmpl_body % ["i", $(i + 10), "data", j, "layout", $(%layout)]
+    html &= h & "</tr>"
+
+
+  html &= "</table></body></html>"
   var
      f: File
   if not open(f, "svvcf.html", fmWrite):
