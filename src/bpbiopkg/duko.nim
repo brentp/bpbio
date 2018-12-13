2f4f18087bedea16d2eb21147bc8bde41c0b3de9
Brent Pedersen <bpederse@gmail.com>
2018-12-12 19:54:42 -0700

    updates to work with variant expr

--- src/bpbiopkg/duko.nim
+++ src/bpbiopkg/duko.nim
@@ -9,9 +9,9 @@ type Duko* = object
 
 type Dukexpr* = object
     ## a compiled expression
+    expr*: string
     ctx*: DTContext
     vptr: pointer
-
 
 converter toBool*(d: duk_bool_t): bool {.inline.} =
     ## automatically converts the duk_bool_t to the appropriate true/false value in nim.
@@ -51,13 +51,13 @@ proc `[]`*(ctx: DTContext, key:string): float {.inline.} =
 
 proc compile*(ctx: DTContext, expression: string): Dukexpr {.inline.} =
   ## compile an expression to be used later. This is about 10X faster than eval_string
-  result = Dukexpr(ctx:ctx)
+  result = Dukexpr(ctx:ctx, expr: expression)
   if ctx.duk_pcompile_string(0, expression) != 0:
     var err = ctx.duk_safe_to_string(-1)
     raise newException(ValueError, $err)
   result.vptr = ctx.duk_get_heapptr(-1)
 
-proc check(d:Dukexpr): bool {.inline.} =
+proc check*(d:Dukexpr): bool {.inline.} =
   ## evaluate a (previously compiled) boolean expression in the current context
   discard d.ctx.duk_push_heapptr(d.vptr)
   if d.ctx.duk_pcall(0) != 0:
@@ -79,16 +79,24 @@ proc newObject*(ctx:DTContext, name: string): Duko =
   result.vptr = ctx.duk_get_heapptr(-1)
   doAssert result.ctx.duk_put_global_string(name)
 
-proc `[]=`*(o: Duko, key:string, value: SomeFloat) {.inline.} =
+proc `[]=`*(o:Duko, key:string, value: SomeFloat) {.inline.} =
     ## set the property at key to a value
     var idx = o.ctx.duk_push_heapptr(o.vptr)
     o.ctx.duk_push_number(value.duk_double_t)
     doAssert o.ctx.duk_put_prop_string(idx, key)
     o.ctx.pop()
 
-proc alias*(o: Duko, copyname:string): Duko {.inline.} =
+proc `[]=`*(o:Duko, key:string, value: SomeInteger) {.inline.} =
+    ## set the property at key to a value
+    var idx = o.ctx.duk_push_heapptr(o.vptr)
+    o.ctx.duk_push_int(value.duk_int_t)
+    if not o.ctx.duk_put_prop_string(idx, key):
+      quit "problem setting:" & key & " -> " & $value
+    o.ctx.pop()
+
+proc alias*(o: Duko, copyname:string): Duko {.inline, discardable.} =
   ## create an alias of a Duko so it can be reference as another name in the javascript.
-  var idx = o.ctx.duk_push_heapptr(o.vptr)
+  doAssert o.ctx.duk_push_heapptr(o.vptr) >= 0
   result = Duko(ctx: o.ctx, name:copyname)
   result.vptr = o.vptr
   doAssert result.ctx.duk_put_global_string(copyname)
@@ -99,13 +107,6 @@ proc clear*(o: var Duko) {.inline.} =
   discard o.ctx.duk_push_object()
   o.vptr = o.ctx.duk_get_heapptr(-1)
   doAssert o.ctx.duk_put_global_string(o.name)
-
-proc `[]=`*(o: Duko, key:string, value: SomeInteger) {.inline.} =
-    ## set the property at key to a value
-    var idx = o.ctx.duk_push_heapptr(o.vptr)
-    o.ctx.duk_push_int(value.duk_int_t)
-    doAssert o.ctx.duk_put_prop_string(idx, key)
-    o.ctx.pop()
 
 proc `[]=`*(o: Duko, key: string, values: seq[SomeNumber]) {.inline.} =
   var idx = o.ctx.duk_push_heapptr(o.vptr)
@@ -125,6 +126,9 @@ proc `[]`*(o: Duko, key:string): float {.inline.} =
 
 when isMainModule:
   import unittest
+  var my_fatal: duk_fatal_function = (proc (udata: pointer, msg:cstring) {.stdcall.} =
+      quit $msg
+  )
 
   suite "duktaper":
     test "usage":
@@ -174,7 +178,7 @@ when isMainModule:
 
       var t = cpuTime()
       when defined(release):
-        var tries = 1_000_000
+        var tries = 500_000
       else:
         var tries = 50_000
 
@@ -197,7 +201,7 @@ when isMainModule:
       ctx.duk_destroy_heap();
 
     test "compiled speed":
-      var ctx = duk_create_heap_default()
+      var ctx = duk_create_heap(nil, nil, nil, nil, my_fatal)
       var expr = "kid.dp < 500 && dad > 21" & " && mom == kid.dp"
       var e = ctx.compile(expr)
       var kid = ctx.newObject("kid")
@@ -232,7 +236,7 @@ when isMainModule:
       var ctx = duk_create_heap_default()
       var kid = ctx.newObject("kid")
       for i in 0..100:
-          kid["some" & $i] = i
+        kid["some" & $i] = i
 
       var kid2 = kid.alias("kid2")
       check kid2["some22"] == 22.0
@@ -243,6 +247,22 @@ when isMainModule:
       check ctx.duk_get_number(-1) == 44.0
       ctx.duk_eval_string("kid2.some22")
       check ctx.duk_get_number(-1) == 44.0
+      ctx.duk_destroy_heap();
 
+    proc addo(obj: Duko) =
+        for i in 0..200:
+          obj["attr" & $i] = i.float
+          obj["attrs" & $i] = @[i.float, i.float*2]
 
-
+    test "many objects":
+      var ctx = duk_create_heap(nil, nil, nil, nil, my_fatal)
+      var objs = newSeq[Duko]()
+      for i in 0..6000:
+        objs.add(ctx.newObject("sample" & $i))
+      for i in 0..10:
+        for obj in objs.mitems:
+          obj.clear
+          obj.addo
+        for obj in objs:
+          obj.addo
+          obj.alias("kid")
